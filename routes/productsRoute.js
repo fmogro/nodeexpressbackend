73b5712262b4e790ece249955c4503b9339393d3
Felipe Mogro <fmogro@gmail.com>
2023-03-12 17:11:38 -0500

    Actualizacion y borrado 	modified:   routes/productsRoute.js 	modified:   services/productService.js

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -20,38 +20,28 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.status(201).json({
-    message: 'Created',
-    data: body,
-  });
+  const newProduct = service.create(body);
+  res.status(201).json(newProduct);
 });
 
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
-    message: 'Patched',
-    data: body,
-    id,
-  });
+  const product = service.update(id, body);
+  res.json(product);
 });
 
 router.put('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
-    message: 'Updated',
-    data: body,
-    id,
-  });
+  const product = service.update(id, body);
+  res.json(product);
 });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    message: 'Deleted',
-    id,
-  });
+  const rta = service.delete(id);
+  res.json(rta);
 });
 
 module.exports = router;
--- services/productService.js
+++ services/productService.js
@@ -10,14 +10,21 @@ class ProductsServices {
     const limit = 10;
     for (let index = 0; index < limit; index++) {
       this.products.push({
-        id: index + 1,
+        id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
         Image: faker.image.imageUrl(),
       });
     }
   }
-  create() {}
+  create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
 
   find() {
     return this.products;
@@ -27,9 +34,27 @@ class ProductsServices {
     return this.products.find((item) => item.id == id);
   }
 
-  update() {}
+  update(id, changes) {
+    const index = this.products.findIndex((item) => item.id == id);
+    if (index === -1) {
+      throw new Error('Producto no encontrado');
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes,
+    };
+    return this.products[index];
+  }
 
-  delete() {}
+  delete(id) {
+    const index = this.products.findIndex((item) => item.id == id);
+    if (index === -1) {
+      throw new Error('Producto no encontrado');
+    }
+    this.products.splice(index, 1);
+    return { message: 'Borrado con exito' };
+  }
 }
 
 module.exports = ProductsServices;
