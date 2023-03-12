0ec57dcaea82f1a40add4f340a177b5af57e671f
Felipe Mogro <fmogro@gmail.com>
2023-03-12 09:18:10 -0500

    Commit inicial, se crea ruta inicial de express Se crea gitignore  Initial commit  Changes to be committed: 	new file:   .editorconfig 	new file:   .eslintrc 	new file:   .gitignore 	new file:   index.js 	new file:   package-lock.json 	new file:   package.json

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server express UP!s');
+});
+
+app.listen(port, () => {
+  console.log('Port used: ' + port);
+});
